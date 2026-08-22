222c34087c0bb85f385f24ac69d85948a7c1a633
lukaszpeciak91-sys <lukaszpeciak91@gmail.com>
2026-08-22 23:21:17 +0200

    Strengthen locale canonicalization contracts

--- app/src/test/java/app/privateaudio/AppLanguagePreferencesTest.kt
+++ app/src/test/java/app/privateaudio/AppLanguagePreferencesTest.kt
@@ -12,6 +12,10 @@ class AppLanguagePreferencesTest {
             "iw" to "he", "iw-IL" to "he-IL",
             "in" to "id", "in-ID" to "id-ID",
             "yi" to "yi", "he" to "he", "id" to "id", "pl-PL" to "pl-PL",
+            "sr-Latn" to "sr-Latn",
+            "az-Arab-IR" to "az-Arab-IR",
+            "zh-Hant-TW" to "zh-Hant-TW",
+            "pt-BR" to "pt-BR",
         ).forEach { (input, expected) ->
             assertEquals(input, expected, AppLanguagePreferences.canonicalLanguageTag(input))
         }
